9a51abe9596c7927c70a9170b69665aa0593bf05
거녀리 맥북 <rjsduf@geonyeoliui-MacBookAir.local>
2022-06-05 03:56:38 +0900

    feat: 서버에 파일 업로드 api 구현 및 파일 형식 변경, 로딩 추가
    파일 업로드 및 script/time 가져오는 api 구현
    파일 형식 변경(only video -> both video and audio), 파일 개수 1개로 제한
    Loading 시 표시
    
    resolve #3

--- src/pages/script/index.tsx
+++ src/pages/script/index.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import { useParams, useLocation } from 'react-router-dom';
+import { styled } from '../../stitches.config';
+import Table from '../../components/table';
 
-export default function Download(): JSX.Element {
-    return <div>Download</div>;
+const Container = styled('div', {
+    width: '70%',
+    height: 'auto',
+    minHeight: 'calc(90vh - 4rem)',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '10vh 15% 0 15%',
+    '@lg': { minHeight: 'calc(90vh - 5rem)', width: '80%', padding: '10vh 10% 0 10%' },
+});
+
+export default function Script(): JSX.Element {
+    const params = useParams();
+    const script: any = useLocation().state;
+    console.log('script', script);
+    console.log(params);
+
+    return <Container>{script ? <Table script={script} /> : <div>1</div>}</Container>;
 }
